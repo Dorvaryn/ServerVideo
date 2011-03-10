e05bccf2853c577d634fbddf2d7405b476ae0f40
Dorvaryn <benjamin.augustin@gmail.com>
2011-03-10 23:01:36 +0100

    Tout premier Push

---
+++ main.c
@@ -0,0 +1,241 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/epoll.h>
+#include <unistd.h> //Pour STDIN_FILENO
+
+#define MAX_EVENTS 10
+#define MAX_HEADER 200
+#define MAX_STR 32
+
+
+char * build_date()
+{
+  return "00 / 00 / 00";
+}
+
+char * build_http_header(char * type, int size)
+{
+  char * header = malloc(MAX_HEADER * sizeof(char));
+  memset(header, '\0', MAX_HEADER);
+  strcat(header, "HTTP/1.1 200 OK\nDate: ");
+  
+  /*Insertion de la date actuelle*/
+  strcat(header, build_date());
+
+  strcat(header, "\nServer: ServLib (Unix) (Arch/Linux)\nAccept-Ranges: bytes\n\
+Content-Length: ");
+
+  /*Insertion de la taille*/
+  char tmp[MAX_STR] = {'\0'};
+  sprintf(tmp, "%d", size);
+  strcat(header, tmp);
+
+  strcat(header, "\nConnection: close\nContent-Type: ");
+
+  /*Insertion du type*/
+  strcat(header, type);
+
+  strcat(header, "; charset=UTF-8\n\n");
+
+  return header;
+}
+
+void file_to_buffer(char ** buff, int * size)
+{
+  /*Ouverture du fichier*/
+  FILE * f = fopen("catalogue.txt", "r");
+  printf("fopen : %s\n", strerror(errno));
+
+  /*Deplacement du curseur à la fin du fichier*/
+  fseek(f, 0,SEEK_END);
+  printf("fseek : %s\n", strerror(errno));
+
+  /*On recupere le nombre de caractere*/
+  *size = ftell(f);
+  printf("fopen : %s\n", strerror(errno));
+
+  int c;
+  int i;
+  /*On alloue un espace memoire de la taille du fichier*/
+  *buff = malloc(*size * sizeof(char));
+
+  /*On se replace au debut du fichier*/
+  fseek(f, 0, SEEK_SET);
+  printf("fseek : %s\n", strerror(errno));
+
+  /*On recupere tout les caracteres*/
+  for(i = 0; i < *size; i++)
+  {
+    c = fgetc(f);
+    (*buff)[i] = c;
+  }
+
+  /*Et on referme le fichier*/
+  fclose(f);
+}
+
+void send_get_answer(int fd)
+{
+  int size;
+  char * buf = NULL;
+  /*On recuper le fichier sous forme de chaine de cara*/
+  file_to_buffer(&buf, &size);
+  /*On construit l'entete HTML aproprié*/
+  char * header = build_http_header("text/plain", size);
+
+  /*Et on envoie les données*/
+  puts("Going to send");
+  send(fd, header, strlen(header), MSG_MORE);
+  printf("send : %s\n", strerror(errno));
+  send(fd, buf, size, 0);
+  printf("send : %s\n", strerror(errno));
+
+  /*On libere les ressources alloué*/
+  free(header);
+  free(buf);
+}
+
+int createSockEvent(int epollfd, int port)
+{
+  int sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  printf("socket : %s\n", strerror(errno));
+
+  struct sockaddr_in saddr;
+ 
+  saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+
+  bind(sock, (struct sockaddr *)&saddr, sizeof(saddr));
+  printf("bind : %s\n", strerror(errno));
+
+  listen(sock, 10);
+  printf("listen : %s\n", strerror(errno));
+
+  struct epoll_event ev;
+
+  ev.events = EPOLLIN | EPOLLET;
+  ev.data.fd = sock;
+  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev) == -1) 
+  {
+    perror("epoll_ctl: sock");
+    exit(EXIT_FAILURE);
+  }
+
+  return sock;
+}
+
+int createSockClientEvent(int epollfd, int sock)
+{
+	int csock;
+	struct epoll_event ev;
+	struct sockaddr_in saddr_client;
+
+	socklen_t size_addr = sizeof(struct sockaddr_in);
+    csock = accept(sock, (struct sockaddr *)&saddr_client, &size_addr);
+    printf("accept : %s\n", strerror(errno));
+
+    printf("Connection de %s :: %d\n", inet_ntoa(saddr_client.sin_addr), 
+    htons(saddr_client.sin_port));
+    if (csock == -1)
+   	{
+	    perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    
+	ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
+    ev.data.fd = csock;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, csock,
+		            &ev) == -1)
+   	{
+	    perror("epoll_ctl: csock");
+        exit(EXIT_FAILURE);
+	}
+
+	return csock;
+}
+
+void central()
+{
+  struct epoll_event ev, events[MAX_EVENTS];
+  int sock, socktest, nfds, epollfd;
+
+  epollfd = epoll_create(10);
+  if (epollfd == -1) {
+    perror("epoll_create");
+    exit(EXIT_FAILURE);
+  }
+  
+  sock = createSockEvent(epollfd, 8081);
+  
+  socktest = createSockEvent(epollfd, 8087);
+  
+  //Ajout à epoll de l'entrée standard
+  ev.events = EPOLLIN;
+  ev.data.fd = STDIN_FILENO;
+  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev) == -1) {
+    perror("epoll_ctl: stdin");
+    exit(EXIT_FAILURE);
+  }
+
+  int done = 0;
+
+  while(!done) //Boucle principale
+  {
+  
+    nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+    puts("working ...");
+    if (nfds == -1) {
+      perror("epoll_pwait");
+      exit(EXIT_FAILURE);
+    }
+	int n,csock,csocktest;
+    for (n = 0; n < nfds; ++n) {
+        if (events[n].data.fd == sock) 
+		{
+			csock = createSockClientEvent(epollfd, sock);	    
+        } else if (events[n].data.fd == socktest)
+	   	{
+			csocktest = createSockClientEvent(epollfd, socktest);
+        } else if(events[n].data.fd == STDIN_FILENO)
+	   	{
+            char chaine[512];
+            scanf("\n%s", chaine); //récupère l'entrée standart
+            
+            //Traite la commande
+            if(strcmp(chaine, "exit") == 0)
+		   	{
+                done = 1;
+			}            
+        }
+		else 
+		{
+            printf("%d\n", events[n].events);
+            if (events[n].data.fd == csock)
+			{
+				if (events[n].events == 5)
+                send_get_answer(events[n].data.fd);
+			}
+            if (events[n].data.fd == csocktest)
+			{
+				if (events[n].events == 5)
+                printf("%s\n","Plop 8087 ok");
+			}
+        }
+    }
+} //Fin de la boucle principale
+  
+  //TODO: fermer les ressources proprement ici
+  
+}  
+
+int main(int argc, char ** argv)
+{
+  central(8081);
+  return 0;
+}
---
+++ utils.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
+//Maximum d'un mot dans les requetes du client
+#define MAX_TOCKEN 256
+
+//Protocoles
+#define TCP_PULL 0
+#define TCP_PUSH 1
+#define UDP_PULL 2
+#define UDP_PUSH 3
+
+//Type de requete
+#define BAD_REQUEST (-2)
+#define NON_DEFINI (-1)
+#define GET 1
+#define START 2
+#define PAUSE 3
+#define END 4
+#define ALIVE 5
+
+//Erreur de converion char->int
+#define PARSE_ERROR -2
+
+//Valeur par défaut de la structure
+#define DEFAULT_REQ {NON_DEFINI, 0, -2, -1, -1, 0, 0, 0, 0, 0, 0}
+
+struct requete {
+    int type;
+    
+    int isOver;
+    
+    int imgId;
+    int listenPort;
+    int fragmentSize;
+    
+    int inWord;
+    int space;
+    int crlfCounter;
+    
+    char* mot;
+    int motPosition; //position dans le mot lu
+    
+    int reqPosition; //position du mot dans la requete
+};
+
+/**
+ * Converti un char* en un entier.
+ * Le char* doit représenter un nombre entre -1 et la taille max
+ * d'un int non signé.
+ */
+int parseInt(char* entier) {
+    if(strcmp(entier, "-1") == 0) {
+        return -1;
+    }
+    
+    int res = 0;
+    int i;
+    for(i = 0; entier[i] != '\0'; i++) {
+        char c = entier[i];
+        if(isdigit(c)) {
+            res = res*10+(c-'0');
+        } else {
+            return PARSE_ERROR;
+        }
+    }
+
+    return res;
+}
+
+void traiteRequete(struct requete* req) {
+    switch(req->type) {
+        case BAD_REQUEST:
+            puts("E: Mauvaise requete");
+            break;
+        case GET:
+            if(req->fragmentSize != -1) {
+                printf("GET id:%d port:%d frag_size:%d\n", req->imgId, req->listenPort, req->fragmentSize);
+            } else if(req->listenPort != -1) {
+                printf("GET id:%d port:%d\n", req->imgId, req->listenPort);
+            } else {
+                printf("GET id:%d\n", req->imgId);
+            }
+            break;
+        case START:
+            printf("START\n");
+            break;
+        case PAUSE:
+            printf("PAUSE\n");
+            break;
+        case END:
+            printf("END\n");
+            break;
+        case ALIVE:
+            printf("ALIVE id:%d port:%d\n", req->imgId, req->listenPort);
+            break;
+        default:
+            break;
+    }
+}
+
+void traiteChaine(char* chaine, struct requete* req) {
+    int i;
+    for(i=0; chaine[i] != '\0' && !req->isOver; i++) {
+        
+        char c = chaine[i];
+    
+        //est-ce que le caractère est un espace ?
+        req->space = (c == ' ' || c == '\n' || c == '\r');
+        
+        if(req->inWord && req->space) { //Le mot est fini, on peut le traiter
+            req->mot[req->motPosition] = '\0';
+            req->inWord = 0;
+            
+            //Traitement du mot lu
+            //puts(req->mot);
+            if(req->reqPosition == 0) {
+                //puts("choix..");
+                if(strcmp(req->mot, "GET") == 0) {
+                    req->type = GET;
+                } else if(strcmp(req->mot, "START") == 0) {
+                    req->type = START;
+                } else if(strcmp(req->mot, "PAUSE") == 0) {
+                    req->type = PAUSE;
+                } else if(strcmp(req->mot, "END") == 0) {
+                    req->type = END;
+                } else if(strcmp(req->mot, "ALIVE") == 0) {
+                    req->type = ALIVE;
+                } else {
+                    req->type = BAD_REQUEST;
+                }
+            } else if(req->reqPosition == 1) {
+                if(req->type != GET && req->type != ALIVE) {
+                    req->type = BAD_REQUEST;
+                } else {
+                    int numero = parseInt(req->mot);
+                    if(numero == PARSE_ERROR) {
+                        req->type = BAD_REQUEST;
+                    } else {
+                        req->imgId = numero;
+                    }
+                }
+            } else if(req->reqPosition == 2 && strcmp(req->mot, "LISTEN_PORT") != 0) {
+                req->type = BAD_REQUEST;
+            } else if(req->reqPosition == 3) {
+                int numero = parseInt(req->mot);
+                if(numero == PARSE_ERROR) {
+                    req->type = BAD_REQUEST;
+                } else {
+                    req->listenPort = numero;
+                }
+            } else if(req->reqPosition == 4) {
+                if(req->type == ALIVE || strcmp(req->mot, "FRAGMENT_SIZE") != 0) {
+                    req->type = BAD_REQUEST;
+                }
+            } else if(req->reqPosition == 5) {
+                int numero = parseInt(req->mot);
+                if(numero == PARSE_ERROR) {
+                    req->type = BAD_REQUEST;
+                } else {
+                    req->fragmentSize = numero;
+                }
+            } else if(req->reqPosition == 6) {
+                req->type = BAD_REQUEST;
+            }
+            
+            req->reqPosition++;
+            
+            if(c=='\r') {
+                req->crlfCounter = 1;
+            }
+            
+        } else if(req->inWord && !req->space) { //Le mot continue
+            req->mot[req->motPosition] = c;
+            req->motPosition++;
+        } else if(!req->inWord && !req->space) { //Le mot commence
+            req->crlfCounter = 0;
+            req->motPosition = 0;
+            req->inWord = 1;
+            req->mot[req->motPosition] = c;
+            req->motPosition++;
+        } else { //Sinon les espaces continuent
+            if( (c == '\r' && (req->crlfCounter == 0 || req->crlfCounter == 2))
+                    || (c == '\n' && (req->crlfCounter == 1 || req->crlfCounter == 3)) ) {
+                req->crlfCounter++;
+            }
+            if(req->crlfCounter == 4) {
+                req->isOver = 1;
+            }
+        }
+        
+        //printf("### %c %d %d %d ###\n", c, req->inWord, req->space, req->motPosition);
+    }
+    
+    if(req->isOver) {
+        free(req->mot);
+        
+        if(req->type == GET && req->imgId == -2) {
+            req->type = BAD_REQUEST;
+        }
+        if(req->type == ALIVE && req->imgId == -2) {
+            req->type = BAD_REQUEST;
+        }
+        /*puts("requete terminée");
+        if(req->type == BAD_REQUEST) {
+            puts("mauvaise requete");
+        } else*/
+        traiteRequete(req);
+    }
+}
+
+int main() {
+    struct requete req = DEFAULT_REQ;
+    
+    /* tests
+    assert(parseInt("1") == 1);
+    assert(parseInt("-1") == -1);
+    assert(parseInt("e") == PARSE_ERROR);
+    assert(parseInt("523654") == 523654);
+    */
+    
+    req.mot = malloc(MAX_TOCKEN*sizeof(char));
+    
+    traiteChaine("ALIV", &req);
+    traiteChaine("E 0 LISTEN_PORT \n  5\r\n \r\n", &req);
+    
+    struct requete req2 = DEFAULT_REQ;
+    req2.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("START\r\n\r\n", &req2);
+    
+    struct requete req3 = DEFAULT_REQ;
+    req3.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("PAUSE\r\n\r\n", &req3);
+    
+    struct requete req4 = DEFAULT_REQ;
+    req4.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("END\r\n\r\n", &req4);
+    
+    struct requete req5 = DEFAULT_REQ;
+    req5.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("GET\r\n\r\n", &req5);
+    
+    struct requete req6 = DEFAULT_REQ;
+    req6.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("GET -1\r\n\r\n", &req6);
+    
+    struct requete req7 = DEFAULT_REQ;
+    req7.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("GET 5\r\n LISTEN_PORT 404\r\n\r\n", &req7);
+    
+    struct requete req8 = DEFAULT_REQ;
+    req8.mot = malloc(MAX_TOCKEN*sizeof(char));
+    traiteChaine("GET 1024\r\n LISTEN_PORT 4096\r\n FRAGMENT_SIZE 32 \r\n\r\n", &req8);
+    
+    //printf("### %d ###\n", req.type);
+    
+    return 0;
+}
