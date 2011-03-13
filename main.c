ae12a1bde676c0f2f6fe28a3399c62b0befeaa09
Dorvaryn <benjamin.augustin@gmail.com>
2011-03-13 18:03:20 +0100

    Test de structure clients sockets TODO: debug gestion requete

--- main.c
+++ main.c
@@ -17,119 +17,119 @@
 
 char * build_date()
 {
-  return "00 / 00 / 00";
+	return "00 / 00 / 00";
 }
 
 char * build_http_header(char * type, int size)
 {
-  char * header = malloc(MAX_HEADER * sizeof(char));
-  memset(header, '\0', MAX_HEADER);
-  strcat(header, "HTTP/1.1 200 OK\nDate: ");
-  
-  /*Insertion de la date actuelle*/
-  strcat(header, build_date());
+	char * header = malloc(MAX_HEADER * sizeof(char));
+	memset(header, '\0', MAX_HEADER);
+	strcat(header, "HTTP/1.1 200 OK\nDate: ");
 
-  strcat(header, "\nServer: ServLib (Unix) (Arch/Linux)\nAccept-Ranges: bytes\n\
-Content-Length: ");
+	/*Insertion de la date actuelle*/
+	strcat(header, build_date());
 
-  /*Insertion de la taille*/
-  char tmp[MAX_STR] = {'\0'};
-  sprintf(tmp, "%d", size);
-  strcat(header, tmp);
+	strcat(header, "\nServer: ServLib (Unix) (Arch/Linux)\nAccept-Ranges: bytes\n\
+			Content-Length: ");
 
-  strcat(header, "\nConnection: close\nContent-Type: ");
+	/*Insertion de la taille*/
+	char tmp[MAX_STR] = {'\0'};
+	sprintf(tmp, "%d", size);
+	strcat(header, tmp);
 
-  /*Insertion du type*/
-  strcat(header, type);
+	strcat(header, "\nConnection: close\nContent-Type: ");
 
-  strcat(header, "; charset=UTF-8\n\n");
+	/*Insertion du type*/
+	strcat(header, type);
 
-  return header;
+	strcat(header, "; charset=UTF-8\n\n");
+
+	return header;
 }
 
 void file_to_buffer(char ** buff, int * size)
 {
-  /*Ouverture du fichier*/
-  FILE * f = fopen("catalogue.txt", "r");
-  printf("fopen : %s\n", strerror(errno));
+	/*Ouverture du fichier*/
+	FILE * f = fopen("catalogue.txt", "r");
+	printf("fopen : %s\n", strerror(errno));
 
-  /*Deplacement du curseur à la fin du fichier*/
-  fseek(f, 0,SEEK_END);
-  printf("fseek : %s\n", strerror(errno));
+	/*Deplacement du curseur à la fin du fichier*/
+	fseek(f, 0,SEEK_END);
+	printf("fseek : %s\n", strerror(errno));
 
-  /*On recupere le nombre de caractere*/
-  *size = ftell(f);
-  printf("fopen : %s\n", strerror(errno));
+	/*On recupere le nombre de caractere*/
+	*size = ftell(f);
+	printf("fopen : %s\n", strerror(errno));
 
-  int c;
-  int i;
-  /*On alloue un espace memoire de la taille du fichier*/
-  *buff = malloc(*size * sizeof(char));
+	int c;
+	int i;
+	/*On alloue un espace memoire de la taille du fichier*/
+	*buff = malloc(*size * sizeof(char));
 
-  /*On se replace au debut du fichier*/
-  fseek(f, 0, SEEK_SET);
-  printf("fseek : %s\n", strerror(errno));
+	/*On se replace au debut du fichier*/
+	fseek(f, 0, SEEK_SET);
+	printf("fseek : %s\n", strerror(errno));
 
-  /*On recupere tout les caracteres*/
-  for(i = 0; i < *size; i++)
-  {
-    c = fgetc(f);
-    (*buff)[i] = c;
-  }
+	/*On recupere tout les caracteres*/
+	for(i = 0; i < *size; i++)
+	{
+		c = fgetc(f);
+		(*buff)[i] = c;
+	}
 
-  /*Et on referme le fichier*/
-  fclose(f);
+	/*Et on referme le fichier*/
+	fclose(f);
 }
 
 void send_get_answer(int fd)
 {
-  int size;
-  char * buf = NULL;
-  /*On recuper le fichier sous forme de chaine de cara*/
-  file_to_buffer(&buf, &size);
-  /*On construit l'entete HTML aproprié*/
-  char * header = build_http_header("text/plain", size);
+	int size;
+	char * buf = NULL;
+	/*On recuper le fichier sous forme de chaine de cara*/
+	file_to_buffer(&buf, &size);
+	/*On construit l'entete HTML aproprié*/
+	char * header = build_http_header("text/plain", size);
 
-  /*Et on envoie les données*/
-  puts("Going to send");
-  send(fd, header, strlen(header), MSG_MORE);
-  printf("send : %s\n", strerror(errno));
-  send(fd, buf, size, 0);
-  printf("send : %s\n", strerror(errno));
+	/*Et on envoie les données*/
+	puts("Going to send");
+	send(fd, header, strlen(header), MSG_MORE);
+	printf("send : %s\n", strerror(errno));
+	send(fd, buf, size, 0);
+	printf("send : %s\n", strerror(errno));
 
-  /*On libere les ressources alloué*/
-  free(header);
-  free(buf);
+	/*On libere les ressources alloué*/
+	free(header);
+	free(buf);
 }
 
 int createSockEvent(int epollfd, int port)
 {
-  int sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-  printf("socket : %s\n", strerror(errno));
+	int sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+	printf("socket : %s\n", strerror(errno));
 
-  struct sockaddr_in saddr;
- 
-  saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  saddr.sin_family = AF_INET;
-  saddr.sin_port = htons(port);
+	struct sockaddr_in saddr;
 
-  bind(sock, (struct sockaddr *)&saddr, sizeof(saddr));
-  printf("bind : %s\n", strerror(errno));
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(port);
 
-  listen(sock, 10);
-  printf("listen : %s\n", strerror(errno));
+	bind(sock, (struct sockaddr *)&saddr, sizeof(saddr));
+	printf("bind : %s\n", strerror(errno));
 
-  struct epoll_event ev;
+	listen(sock, 10);
+	printf("listen : %s\n", strerror(errno));
 
-  ev.events = EPOLLIN | EPOLLET;
-  ev.data.fd = sock;
-  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev) == -1) 
-  {
-    perror("epoll_ctl: sock");
-    exit(EXIT_FAILURE);
-  }
+	struct epoll_event ev;
 
-  return sock;
+	ev.events = EPOLLIN | EPOLLET;
+	ev.data.fd = sock;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sock, &ev) == -1) 
+	{
+		perror("epoll_ctl: sock");
+		exit(EXIT_FAILURE);
+	}
+
+	return sock;
 }
 
 int createSockClientEvent(int epollfd, int sock)
@@ -139,24 +139,24 @@ int createSockClientEvent(int epollfd, int sock)
 	struct sockaddr_in saddr_client;
 
 	socklen_t size_addr = sizeof(struct sockaddr_in);
-    csock = accept(sock, (struct sockaddr *)&saddr_client, &size_addr);
-    printf("accept : %s\n", strerror(errno));
+	csock = accept(sock, (struct sockaddr *)&saddr_client, &size_addr);
+	printf("accept : %s\n", strerror(errno));
 
-    printf("Connection de %s :: %d\n", inet_ntoa(saddr_client.sin_addr), 
-    htons(saddr_client.sin_port));
-    if (csock == -1)
-   	{
-	    perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    
+	printf("Connection de %s :: %d\n", inet_ntoa(saddr_client.sin_addr), 
+			htons(saddr_client.sin_port));
+	if (csock == -1)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
 	ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
-    ev.data.fd = csock;
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, csock,
-		            &ev) == -1)
-   	{
-	    perror("epoll_ctl: csock");
-        exit(EXIT_FAILURE);
+	ev.data.fd = csock;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, csock,
+				&ev) == -1)
+	{
+		perror("epoll_ctl: csock");
+		exit(EXIT_FAILURE);
 	}
 
 	return csock;
@@ -164,102 +164,106 @@ int createSockClientEvent(int epollfd, int sock)
 
 void central()
 {
-  struct epoll_event ev, events[MAX_EVENTS];
-  int sock, socktest, nfds, epollfd;
+	struct epoll_event ev, events[MAX_EVENTS];
+	int sock, socktest, nfds, epollfd;
 
-  epollfd = epoll_create(10);
-  if (epollfd == -1) {
-    perror("epoll_create");
-    exit(EXIT_FAILURE);
-  }
-  
-  sock = createSockEvent(epollfd, 8081);
-  
-  socktest = createSockEvent(epollfd, 8087);
-  
-  //Ajout à epoll de l'entrée standard
-  ev.events = EPOLLIN;
-  ev.data.fd = STDIN_FILENO;
-  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev) == -1) {
-    perror("epoll_ctl: stdin");
-    exit(EXIT_FAILURE);
-  }
+	epollfd = epoll_create(10);
+	if (epollfd == -1) {
+		perror("epoll_create");
+		exit(EXIT_FAILURE);
+	}
 
-  int done = 0;
+	sock = createSockEvent(epollfd, 8081);
 
-  struct stockClient {
-	  int nbClients;
-	  struct requete * requetes;
-	  };
+	socktest = createSockEvent(epollfd, 8087);
 
-  struct stockClient tabClient;
-  tabClient.requetes = (struct requete *)malloc(10*sizeof(struct requete));
-  tabClient.nbClients = 0;
+	//Ajout à epoll de l'entrée standard
+	ev.events = EPOLLIN;
+	ev.data.fd = STDIN_FILENO;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev) == -1) {
+		perror("epoll_ctl: stdin");
+		exit(EXIT_FAILURE);
+	}
 
-  while(!done) //Boucle principale
-  {
-  
-    nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-    puts("working ...");
-    if (nfds == -1) {
-      perror("epoll_pwait");
-      exit(EXIT_FAILURE);
-    }
-	int n,csock,csocktest;
-    for (n = 0; n < nfds; ++n) {
-        if (events[n].data.fd == sock) 
-		{
-			csock = createSockClientEvent(epollfd, sock);	    
-        }
-	   	else if (events[n].data.fd == socktest)
-	   	{
+	int done = 0;
 
-			csocktest = createSockClientEvent(epollfd, socktest);
-			tabClient.nbClients++;
-			tabClient.requetes[tabClient.nbClients-1].sock = csocktest;
-        }
-	   	else if(events[n].data.fd == STDIN_FILENO)
-	   	{
-            char chaine[512];
-            scanf("\n%s", chaine); //récupère l'entrée standart
-            
-            //Traite la commande
-            if(strcmp(chaine, "exit") == 0)
-		   	{
-                done = 1;
-			}            
-        }
-		else 
-		{
-            printf("%d\n", events[n].events);
-            if (events[n].data.fd == csock)
+	struct sockClient {
+		int sock;
+		struct requete requete;
+	};
+	struct tabClients {
+		int nbClients;
+		struct sockClient * clients;
+	};
+
+	struct tabClients tabClient;
+	tabClient.clients = (struct sockClient *)malloc(10*sizeof(struct sockClient));
+	tabClient.nbClients = 0;
+
+	while(!done) //Boucle principale
+	{
+
+		nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+		puts("working ...");
+		if (nfds == -1) {
+			perror("epoll_pwait");
+			exit(EXIT_FAILURE);
+		}
+		int n,csock,csocktest;
+		for (n = 0; n < nfds; ++n) {
+			if (events[n].data.fd == sock) 
 			{
-				if (events[n].events == 5)
-                send_get_answer(events[n].data.fd);
+				csock = createSockClientEvent(epollfd, sock);	    
 			}
-            if (events[n].data.fd == csocktest)
+			else if (events[n].data.fd == socktest)
 			{
-				if (events[n].events == 5)
-				{					
-					printf("%s\n", "buffer");
-					char buffer[512];
-					printf("%s\n", "recv");
-					recv(csocktest, buffer, 512*sizeof(char),0);
-					printf("%s\n", "traite");
-					traiteChaine(buffer, &tabClient.requetes[1]);
-					printf("%s\n", "over");
+
+				csocktest = createSockClientEvent(epollfd, socktest);
+				tabClient.nbClients++;
+				tabClient.clients[tabClient.nbClients-1].sock = csocktest;
+			}
+			else if(events[n].data.fd == STDIN_FILENO)
+			{
+				char chaine[512];
+				scanf("\n%s", chaine); //récupère l'entrée standart
+
+				//Traite la commande
+				if(strcmp(chaine, "exit") == 0)
+				{
+					done = 1;
+				}            
+			}
+			else 
+			{
+				printf("%d\n", events[n].events);
+				if (events[n].data.fd == csock)
+				{
+					if (events[n].events == 5)
+						send_get_answer(events[n].data.fd);
+				}
+				if (events[n].data.fd == csocktest)
+				{
+					if (events[n].events == 5)
+					{					
+						printf("%s\n", "buffer");
+						char buffer[512];
+						printf("%s\n", "recv");
+						recv(csocktest, buffer, 512*sizeof(char),0);
+						printf("%s\n", "traite");
+						traiteChaine(buffer, &tabClient.clients[1].requete);
+						printf("%s\n", "over");
+					}
 				}
 			}
-        }
-    }
-} //Fin de la boucle principale
-  
-  //TODO: fermer les ressources proprement ici
-  
+		}
+	} //Fin de la boucle principale
+
+	//TODO: fermer les ressources proprement ici
+
 }  
 
 int main(int argc, char ** argv)
 {
-  central(8081);
-  return 0;
+	central(8081);
+	return 0;
 }
